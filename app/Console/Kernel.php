1bb9e9be0e48eb82c6559877ed83c5baa341c20a
rizzu1023 <mrizwan9022@gmail.com>
2021-08-11 22:01:30 +0530

    cron jobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('optimize');
+        $schedule->command('config:clear')->everyMinute();
+        $schedule->command('config:cache')->everyTwoMinutes();
+        $schedule->command('view:clear')->everyThreeMinutes();
+        $schedule->command('telescope:clear')->daily();
     }
 
     /**
