ae44a84f08018fa780c7801ae98f682a24b1f665
rizzu1023 <mrizwan9022@gmail.com>
2021-08-14 18:35:12 +0530

    fixed

--- app/Listeners/resetInningListener.php
+++ app/Listeners/resetInningListener.php
@@ -32,65 +32,73 @@ class resetInningListener
      */
     public function handle($event)
     {
-        Game::where('match_id',$event->request->match_id)
-            ->where('tournament_id',$event->request->tournament)
-            ->decrement('status');
 
-        $batting_team = MatchDetail::where('match_id',$event->request->match_id)
+        $game = Game::where('match_id',$event->request->match_id)
             ->where('tournament_id',$event->request->tournament)
-            ->where('team_id',$event->request->bt_team_id)
             ->first();
 
-        $batting_team->score = 0;
-        $batting_team->wicket = 0;
-        $batting_team->over = 0;
-        $batting_team->overball = 0;
-        $batting_team->no_ball = 0;
-        $batting_team->wide = 0;
-        $batting_team->byes = 0;
-        $batting_team->legbyes = 0;
-        $batting_team->isOver = 0;
-        $batting_team->save();
+        if($game->status == 1 || $game->status == 3){
+            $game->status = $game->status - 1;
+            $game->update();
 
-        $batting_team_players = MatchPlayers::where('match_id', $event->request->match_id)
-            ->where('tournament_id', $event->request->tournament)
-            ->where('team_id', $event->request->bt_team_id)->get();
+            $batting_team = MatchDetail::where('match_id',$event->request->match_id)
+                ->where('tournament_id',$event->request->tournament)
+                ->where('team_id',$event->request->bt_team_id)
+                ->first();
 
-        $bowling_team_players = MatchPlayers::where('match_id', $event->request->match_id)
-            ->where('tournament_id', $event->request->tournament)
-            ->where('team_id', $event->request->bw_team_id)->get();
+            $batting_team->score = 0;
+            $batting_team->wicket = 0;
+            $batting_team->over = 0;
+            $batting_team->overball = 0;
+            $batting_team->no_ball = 0;
+            $batting_team->wide = 0;
+            $batting_team->byes = 0;
+            $batting_team->legbyes = 0;
+            $batting_team->isOver = 0;
+            $batting_team->save();
 
-        foreach ($batting_team_players as $player){
-            $player->bt_status = 'DNB';
-            $player->bt_runs = 0;
-            $player->bt_balls = 0;
-            $player->bt_fours = 0;
-            $player->bt_sixes = 0;
-            $player->bt_order = 100;
-            $player->wicket_type = '--';
-            $player->wicket_primary = '--';
-            $player->wicket_secondary = '--';
-            $player->save();
+            $batting_team_players = MatchPlayers::where('match_id', $event->request->match_id)
+                ->where('tournament_id', $event->request->tournament)
+                ->where('team_id', $event->request->bt_team_id)->get();
+
+            $bowling_team_players = MatchPlayers::where('match_id', $event->request->match_id)
+                ->where('tournament_id', $event->request->tournament)
+                ->where('team_id', $event->request->bw_team_id)->get();
+
+            foreach ($batting_team_players as $player){
+                $player->bt_status = 'DNB';
+                $player->bt_runs = 0;
+                $player->bt_balls = 0;
+                $player->bt_fours = 0;
+                $player->bt_sixes = 0;
+                $player->bt_order = 100;
+                $player->wicket_type = '--';
+                $player->wicket_primary = '--';
+                $player->wicket_secondary = '--';
+                $player->save();
+            }
+
+            foreach ($bowling_team_players as $player){
+                $player->bw_status = 'DNB';
+                $player->bw_over = 0;
+                $player->bw_overball = 0;
+                $player->bw_wickets = 0;
+                $player->bw_runs = 0;
+                $player->bw_maiden = 0;
+                $player->bw_nb = 0;
+                $player->bw_wide = 0;
+                $player->save();
+            }
+
+            $batting_team_match_track = MatchTrack::where('match_id', $event->request->match_id)
+                ->where('tournament_id', $event->request->tournament)
+                ->where('team_id', $event->request->bt_team_id)->get();
+
+            foreach ($batting_team_match_track as $mt){
+                $mt->delete();
+            }
         }
 
-        foreach ($bowling_team_players as $player){
-            $player->bw_status = 'DNB';
-            $player->bw_over = 0;
-            $player->bw_overball = 0;
-            $player->bw_wickets = 0;
-            $player->bw_runs = 0;
-            $player->bw_maiden = 0;
-            $player->bw_nb = 0;
-            $player->bw_wide = 0;
-            $player->save();
-        }
 
-        $batting_team_match_track = MatchTrack::where('match_id', $event->request->match_id)
-            ->where('tournament_id', $event->request->tournament)
-            ->where('team_id', $event->request->bt_team_id)->get();
-
-        foreach ($batting_team_match_track as $mt){
-            $mt->delete();
-        }
     }
 }
